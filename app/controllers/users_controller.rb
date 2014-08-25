28c3e25e90531eb56f5bedc0b5b1bb52a3b660c6
App Academy Student <student@appacademy.io>
2014-08-25 15:25:12 -0700

    API working: create, show, update, destroy. Refactored and tested.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,45 @@
 class UsersController < ApplicationController
   def index
-    render text: "I'm in the index action!"
+    # render text: "I'm in the index action!"
+    render json: User.all
   end
+  
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render(
+        json: user.errors.full_messages, status: :unprocessable_entity
+      )
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update!(user_params)
+      render json: @user
+    else
+      render(
+        json: user.errors.full_messages, status: :unprocessable_entity
+      )
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    
+    render json: { message: 'Destroyed!' } 
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+  
 end
--- bin/my_script.rb
+++ bin/my_script.rb
@@ -1,13 +1,63 @@
+#!/usr/bin/env ruby 
+
 # bin/my_script.rb
 
 require "addressable/uri"
 require "rest-client"
 
-url = Addressable::URI.new(
-  scheme: 'http',
-  host: 'localhost',
-  port: 3000,
-  path: '/users.html'
-).to_s
+# url = Addressable::URI.new(
+#   scheme: 'http',
+#   host: 'localhost',
+#   port: 3000,
+#   path: '/users.html'
+# ).to_s
+#
+# puts RestClient.get(url)
 
-puts RestClient.get(url)
+def create_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users.json'
+  ).to_s
+
+  puts RestClient.post(
+    url,
+    { user: { email: "gizmo@gizmo.gizmo" } }
+    # { user: { name: "Gizmo", email: "gizmo@gizmo.gizmo" } }
+  )
+end
+ 
+#  patch 'users/:id'     => 'users#update'  
+def update_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users/1'
+  ).to_s
+
+  puts RestClient.put(
+    url,
+    { user: { email: "c-still-broke@coolstuff.com" } } )
+  
+end
+
+def delete_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users/6'
+  ).to_s
+
+  puts RestClient.delete(
+    url ) #,
+    #{ user: { id: 6 } } )
+end
+
+# create_user
+update_user
+#delete_user
+
