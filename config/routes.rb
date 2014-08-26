082239a354bb6bdab6ba7765dbacdc17b5af48ca
App Academy Student <student@appacademy.io>
2014-08-25 18:00:41 -0700

    Began ContactShares API

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :destroy, :index, :show, :update]
   resources :contacts, only: [:create, :destroy, :index, :show, :update]
-  
+  resources :contact_shares, only: [:create, :destroy, :index, :show, :update]
 end
 
